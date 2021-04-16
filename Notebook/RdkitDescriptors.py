37b0083b2c168753e9962ea98d22f9301f6c201f
Kjetil Dyrland <kjetil.dyrland@gmail.com>
2021-04-16 17:09:21 +0200

    ECFP

---
+++ Notebook/RdkitDescriptors.py
@@ -0,0 +1,8 @@
+from rdkit import Chem
+from rdkit.Chem import AllChem
+import csv
+import os
+
+
+def ECFP (ifile, ofile, diam, bits): 
+    
--- dataset/13321_2017_226_MOESM1_ESM/RdkitDescriptors.py
+++ dataset/13321_2017_226_MOESM1_ESM/RdkitDescriptors.py
@@ -3,28 +3,30 @@ from rdkit import Chem
 from rdkit.Chem import AllChem
 import csv
 import os
+from pathlib import Path
 
 
+path = Path('./')
 
 
 def ECFP(ifile,ofile,diam,bits):
     """
     Generates circular fingerprints ECFP4 hashed into n bits length vectors
     """
-    reader=csv.reader(open(ifile,'rb'),delimiter=',',quotechar='"')
-    print "Now reading data from file: {}".format(ifile)
-    writer=csv.writer(open(ofile,'wb'),delimiter=',')
+    reader=csv.reader(open(ifile,'r'),delimiter=',',quotechar='"')
+    print("Now reading data from file: {}".format(ifile))
+    writer=csv.writer(open(ofile,'w'),delimiter=',')
     count=0
-    headers=reader.next()
-    print "Headers: {}".format(headers)
+    headers=next(reader)
+    print ("Headers: {}".format(headers))
     ECFP=[]
     for j in range(1024):
-	ECFP.append("ECFP4_"+str(j+1))
+	    ECFP.append("ECFP4_"+str(j+1))
     writer.writerow(headers+ECFP)
     for row in reader:
         count+=1
-	if count%1000==0:
-            print count
+        if count%1000==0:
+            print (count)
         try:
             mol=Chem.MolFromSmiles(str(row[1]))
             Chem.SanitizeMol(mol)
@@ -32,15 +34,15 @@ def ECFP(ifile,ofile,diam,bits):
             temp=[]
             for j in range(len(fp)):
                 temp.append(fp[j])
-            writer.writerow([row[0],row[1],row[2]]+temp)
+                writer.writerow([row[0],row[1],row[2]]+temp)
         except:
-            print row[0],row[1]
-            pass
+                print (row[0],row[1])
+        pass
 
     return
 
 
 
 if __name__ == '__main__':
-    ECFP("CHEMBL218_cl.csv",
-         "CHEMBL218_cl_ecfp_1024.csv",2,1024)
+    ECFP("./dataset/13321_2017_226_MOESM1_ESM/CHEMBL218_cl.csv",
+         "./dataset/13321_2017_226_MOESM1_ESM/CHEMBL218_cl_ecfp_1024.csv",2,1024)
